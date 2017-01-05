62bdb086209c3b203ae7804fb98afeadfa03d2ac
aaronmams <aaron.mamula@noaa.gov>
2017-01-04 16:37:10 -0800

    remove credentials

--- R/primary_data.R
+++ R/primary_data.R
@@ -1,6 +1,6 @@
 
 library(RODBC)
-channel<- odbcConnect(dsn="pacfin",uid="amamula",pw="mam2pac$",believeNRows=FALSE)
+channel<- odbcConnect(dsn="pacfin",uid="",pw="",believeNRows=FALSE)
 vc <- sqlQuery(channel,"select VESSEL_NUM, PACFIN_YEAR, max(CG_VESSEL_LENGTH), max(VESSEL_LENGTH)
 FROM PACFIN_MARTS.SWFSC_FISH_TICKETS
 GROUP BY VESSEL_NUM, PACFIN_YEAR" )
