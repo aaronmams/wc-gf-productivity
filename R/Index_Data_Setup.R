2919dbb185ed9602db9492c0c91f12b98fd44eaf
aaronmams <aaron.mamula@noaa.gov>
2017-01-17 10:02:01 -0800

    merge lbs and value with fish tickets

--- R/Index_Data_Setup.R
+++ R/Index_Data_Setup.R
@@ -7,20 +7,19 @@
 #read in the output file
 
 catch <- readRDS('data/catch_by_ftid.RDA')
-
+lb <- readRDS('data/lb_trip.RDA')
 ########################################################################
 ########################################################################
 ########################################################################
 ########################################################################
 ########################################################################
 ########################################################################
-
 
 #merge species aggregates with catch data frame and declare any species not
 # included in the grouping as 'OTH'
 names(species.agg) <- c('PACFIN_SPECIES_CODE','GROUP')
 catch <- tbl_df(catch) %>% left_join(species.agg,by=c('PACFIN_SPECIES_CODE')) %>%
-            mutate(GROUP=ifelse(is.na(GROUP),'OTH','GROUP'))
+            mutate(GROUP=ifelse(is.na(GROUP),'OTH',as.character(GROUP)))
 
 #aggregate by vessel and fish ticket id
 catch <- catch %>% group_by(VESSEL_NUM, FTID, GROUP) %>% 
@@ -36,7 +35,7 @@ names(catch) <- c('DRVID','FTID','GROUP','lbs','val')
 ########################################################################
 #next join the fish ticket lbs and value to the logbooks
 
-lb <- lb %>% left_join(catch,by=c('DRVID','FTID')) %>%
+lb <- lb %>% left_join(catch,by=c('DRVID','FTID'))
 
 ########################################################################
 ########################################################################
